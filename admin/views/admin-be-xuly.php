0f7477eb615d4cacdb6c73279765d2d653807a77
yen.ly <ntyly.httt0115@student.ctuet.edu.vn>
2019-05-09 20:07:03 +0700

    nhut

--- admin/views/admin-be-xuly.php
+++ admin/views/admin-be-xuly.php
@@ -22,7 +22,9 @@ if(isset($_POST['get_data_lop_hoc'])) {
 
 
 if(isset($_GET['load_list_be'])) {
-    $str = "SELECT * FROM be INNER JOIN lophoc_be ON be.id = lophoc_be.be_id INNER JOIN lophoc_chitiet ON lophoc_be.lop_hoc_chi_tiet_id = lophoc_chitiet.id ORDER BY be.id DESC";
+    $str = "SELECT be.id,be.ten,be.ngaysinh,be.gioitinh,be.chieucao,lophoc_chitiet.mo_ta,be.cannang,be.diachi,be.tinhtrangsuckhoe,be.benhbamsinh,be.hinhbe,be.tencha,be.sdtcha,be.tenme,be.sdtme,be.matracuu,be.chieucao,be.trangthai 
+            FROM be INNER JOIN lophoc_be ON be.id = lophoc_be.be_id 
+            INNER JOIN lophoc_chitiet ON lophoc_be.lop_hoc_chi_tiet_id = lophoc_chitiet.id ORDER BY be.id DESC";
     $query = mysqli_query($dbc, $str);
     $result = array();
 
