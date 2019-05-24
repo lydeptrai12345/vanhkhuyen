f14db8bfc7a05053257e68968d0eb61493165343
lam.tran <lamtranduc2704@gmail.com>
2019-05-24 22:07:17 +0700

    tắt câu query

--- admin/js/nhap.nguyen.lieu.js
+++ admin/js/nhap.nguyen.lieu.js
@@ -1,4 +1,7 @@
-
+Number.prototype.format = function(n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
 $(document).ready(function () {
 
     $('.date_nguyen_lieu').datepicker({
